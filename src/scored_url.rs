ef97a45914383a5b551cbc699c11e207c0b9bc3b
Sawawada8 <sawadadamskz7575@gmail.com>
2025-10-07 23:55:58 +0900

    add path score

--- src/scored_url.rs
+++ src/scored_url.rs
@@ -31,20 +31,46 @@ impl ScoredUrl {
     }
 
     pub fn calc_score(&self) -> Self {
-        let original_host = self.original_url.host_str().unwrap_or("");
-        let url_host = self.url.host_str().unwrap_or("");
-        let score = if original_host == url_host {
-            10
-        } else if url_host.ends_with(original_host) {
-            5
-        } else {
-            1
-        };
+        let mut score = self.calc_score_from_domain();
+        score += self.calc_score_from_path();
         Self {
             original_url: self.original_url.clone(),
             url: self.url.clone(),
             score: Some(score),
         }
+    }
+
+    fn calc_score_from_domain(&self) -> u32 {
+        let original_host = self.original_url.host_str().unwrap_or("");
+        let url_host = self.url.host_str().unwrap_or("");
+        if original_host == url_host {
+            10
+        } else if url_host.ends_with(original_host) {
+            5
+        } else {
+            1
+        }
+    }
+
+    const PATH_SCORE_UNIT: u32 = 2;
+    fn calc_score_from_path(&self) -> u32 {
+        let original_pathes = self
+            .original_url
+            .path_segments()
+            .unwrap()
+            .collect::<Vec<_>>();
+        let url_pathes = self.url.path_segments().unwrap().collect::<Vec<_>>();
+        let loop_count = url_pathes.len().min(original_pathes.len());
+        let mut c = 0;
+        for i in 0..loop_count {
+            let o_seg = original_pathes[i];
+            let u_seg = url_pathes[i];
+            if o_seg != u_seg {
+                return c * Self::PATH_SCORE_UNIT;
+            }
+            c += 1;
+        }
+        c * Self::PATH_SCORE_UNIT
     }
 }
 
@@ -55,10 +81,11 @@ mod tests {
 
     #[test]
     fn test_calc_score() {
-        let original_url = Url::parse("https://example.com").unwrap();
+        let original_url = Url::parse("https://example.com/a").unwrap();
         let url_same = Url::parse("https://example.com/page").unwrap();
         let url_subdomain = Url::parse("https://sub.example.com").unwrap();
         let url_different = Url::parse("https://different.com").unwrap();
+        let same_path = Url::parse("https://different.com/a").unwrap();
 
         let scored_same = ScoredUrl::new(original_url.clone(), url_same).calc_score();
         assert_eq!(scored_same.score, Some(10));
@@ -66,7 +93,10 @@ mod tests {
         let scored_subdomain = ScoredUrl::new(original_url.clone(), url_subdomain).calc_score();
         assert_eq!(scored_subdomain.score, Some(5));
 
-        let scored_different = ScoredUrl::new(original_url, url_different).calc_score();
+        let scored_different = ScoredUrl::new(original_url.clone(), url_different).calc_score();
         assert_eq!(scored_different.score, Some(1));
+
+        let scored_different = ScoredUrl::new(original_url, same_path).calc_score();
+        assert_eq!(scored_different.score, Some(1 + 2));
     }
 }
